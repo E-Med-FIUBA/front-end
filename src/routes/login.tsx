4c5c591272eb5bc6bd26565f3741644a56449b75
Manuel Sanchez <manuelsfdelavega@gmail.com>
2024-06-22 20:48:30 -0300

    Update login form

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,18 +1,19 @@
-import { Button, LabeledInput } from "@/components";
+import { Button, Input } from "@/components";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
-    <div
-      id="login"
-      className="w-svw h-svh flex flex-col justify-center items-center"
-    >
-      <h1>E-Med</h1>
-      <form>
-        <LabeledInput label="Username:" />
-        <br />
-        <LabeledInput label="Password:" />
-        <br />
+    <div className="container lg:max-w-5xl h-svh flex justify-around items-center">
+      <div className="hidden lg:block w-1/4">
+        <img src="/public/vite.svg" alt="Doctor" className="w-full" />
+      </div>
+      <form className="grid gap-2 w-full max-w-80">
+        <Input type="text" placeholder="Correo electronico" />
+        <Input type="text" placeholder="Correo electronico" />
         <Button type="submit">Login</Button>
+        <Button type="button" asChild>
+          <Link to="/register">Register</Link>
+        </Button>
       </form>
     </div>
   );
