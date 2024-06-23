0102fa3f888d3bd2f3e4b9a75133aa05acff74b1
Manuel Sanchez <manuelsfdelavega@gmail.com>
2024-06-23 01:33:26 -0300

    separate router config from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,56 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import {
-  Navigate,
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
-import ErrorPage from "./ErrorPage";
-import {
-  ProtectedRoute,
-  Dashboard,
-  UnprotectedRoute,
-  Login,
-  Root,
-  Header,
-} from "./routes";
-
-const router = createBrowserRouter([
-  {
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        element: <ProtectedRoute />,
-        children: [
-          {
-            element: <Header />,
-            children: [
-              {
-                path: "/dashboard",
-                element: <Dashboard />,
-              },
-            ],
-          },
-        ],
-      },
-      {
-        element: <UnprotectedRoute />,
-        children: [
-          {
-            path: "/login",
-            element: <Login />,
-          },
-        ],
-      },
-      {
-        path: "/",
-        element: <Navigate to="/dashboard" replace />,
-      },
-    ],
-  },
-]);
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
---
+++ src/router.tsx
@@ -0,0 +1,51 @@
+import {
+  Navigate,
+  createBrowserRouter,
+} from "react-router-dom";
+import ErrorPage from "./ErrorPage";
+import {
+  ProtectedRoute,
+  Dashboard,
+  UnprotectedRoute,
+  Login,
+  Root,
+  Header,
+} from "./routes";
+
+const router = createBrowserRouter([
+  {
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        element: <ProtectedRoute />,
+        children: [
+          {
+            element: <Header />,
+            children: [
+              {
+                path: "/dashboard",
+                element: <Dashboard />,
+              },
+            ],
+          },
+        ],
+      },
+      {
+        element: <UnprotectedRoute />,
+        children: [
+          {
+            path: "/login",
+            element: <Login />,
+          },
+        ],
+      },
+      {
+        path: "/",
+        element: <Navigate to="/dashboard" replace />,
+      },
+    ],
+  },
+]);
+
+export default router;
