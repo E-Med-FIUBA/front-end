1eec6ceddcedb343861315830b1e4f3a34f1e023
Manuel Sanchez <manuelsfdelavega@gmail.com>
2024-06-22 23:03:15 -0300

    replace /public/vite.svg to vite.svg

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -5,7 +5,7 @@ export default function Login() {
   return (
     <div className="container lg:max-w-5xl h-svh flex justify-around items-center">
       <div className="hidden lg:block w-1/4">
-        <img src="/public/vite.svg" alt="Doctor" className="w-full" />
+        <img src="/vite.svg" alt="Doctor" className="w-full" />
       </div>
       <form className="grid gap-2 w-full max-w-80">
         <Input type="text" placeholder="Correo electronico" />
