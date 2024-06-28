eb5230406cb51228c015fc4d12afa422ed7a254d
Manuel Sanchez <manuelsfdelavega@gmail.com>
2024-06-28 20:35:37 -0300

    Add no restricted paths rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,31 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "import"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "import/no-restricted-paths": [
+      "error",
+      {
+        zones: [
+          // disables cross-feature imports:
+          // eg. src/features/discussions should not import from src/features/comments, etc.
+          {
+            target: "./src/features/auth",
+            from: "./src/features",
+            except: ["./auth"],
+          },
+        ],
+      },
+    ],
   },
-}
+};
