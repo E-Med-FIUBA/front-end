8e291c6c5cca383e988612dc09aede99d7c1b37f
Manuel Sanchez <manuelsfdelavega@gmail.com>
2024-06-30 17:14:27 -0300

    Fix content layout

--- src/components/layouts/ContentLayout.tsx
+++ src/components/layouts/ContentLayout.tsx
@@ -8,11 +8,11 @@ type ContentLayoutProps = {
 export const ContentLayout = ({ children, title }: ContentLayoutProps) => {
   return (
     <>
-      <div className="py-6">
+      <div className="py-6 h-full flex flex-col">
         <div className="mx-auto container px-4 sm:px-6 md:px-8">
           <h1 className="text-2xl font-semibold text-gray-900">{title}</h1>
         </div>
-        <div className="mx-auto container px-4 py-6 sm:px-6 md:px-8">
+        <div className="mx-auto container px-4 py-6 sm:px-6 md:px-8 flex-1">
           {children}
         </div>
       </div>
