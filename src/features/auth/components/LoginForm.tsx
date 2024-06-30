cf708d9d391d1619699253ec2f8857192d7ba592
Manuel Sanchez <manuelsfdelavega@gmail.com>
2024-06-30 02:12:12 -0300

    Update login form

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -1,52 +1,47 @@
 import { Link } from "react-router-dom";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { SubmitHandler } from "react-hook-form";
 import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { useAuth } from "@/hooks/useAuth";
 import { userDataMock } from "@/testing/mocks/userData";
+import { Form } from "@/components/ui/Form";
 
-const schema = z.object({
+const loginSchema = z.object({
   email: z.string(),
   password: z.string(),
 });
 
-type LoginFormInputs = z.infer<typeof schema>;
+type LoginFormInputs = z.infer<typeof loginSchema>;
 
 export function LoginForm() {
   const { login } = useAuth();
-  const {
-    register,
-    handleSubmit,
-  } = useForm<LoginFormInputs>({
-    resolver: zodResolver(schema),
-  });
 
   const onValid: SubmitHandler<LoginFormInputs> = (data) => {
     login(userDataMock);
   };
 
   return (
-    <form
-      className="grid gap-2 w-full max-w-80"
-      onSubmit={handleSubmit(onValid)}
-    >
-      <Input
-        type="text"
-        placeholder="Correo electronico"
-        {...register("email")}
-      />
-      <Input
-        type="password"
-        placeholder="Contraseña"
-        {...register("password")}
-      />
-      <Button type="submit">Iniciar sesion</Button>
-      <Button type="button" variant="outline" asChild>
-        <Link to="/register">Crear cuenta nueva</Link>
-      </Button>
-    </form>
+    <Form className="grid gap-2 w-full max-w-80" onSubmit={onValid} schema={loginSchema}>
+      {({ register }) => (
+        <>
+          <Input
+            type="text"
+            placeholder="Correo electronico"
+            {...register("email")}
+          />
+          <Input
+            type="password"
+            placeholder="Contraseña"
+            {...register("password")}
+          />
+          <Button type="submit">Iniciar sesion</Button>
+          <Button type="button" variant="outline" asChild>
+            <Link to="/register">Crear cuenta nueva</Link>
+          </Button>
+        </>
+      )}
+    </Form>
   );
 }
