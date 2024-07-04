82ba47026d1d7ecb5afc9c9f865088836e90629e
Manuel Sanchez <manuelsfdelavega@gmail.com>
2024-07-04 01:45:39 -0300

    Basic prescription form

---
+++ src/components/ui/combobox.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { cn } from "@/utils/cn";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+interface ComboboxItem {
+  label: string;
+  value: string;
+}
+
+export function Combobox({ values: items, placeholder, emptyMessage }: {
+    values: ComboboxItem[];
+    placeholder: string;
+    emptyMessage: string;
+}) {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="justify-between"
+        >
+          {value
+            ? items.find((item) => item.value === value)?.label
+            : placeholder}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="p-0">
+        <Command>
+          <CommandInput placeholder={placeholder} />
+          <CommandList>
+            <CommandEmpty>{emptyMessage}</CommandEmpty>
+            <CommandGroup>
+              {items.map((item) => (
+                <CommandItem
+                  key={item.value}
+                  value={item.value}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? "" : currentValue);
+                    setOpen(false);
+                  }}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      value === item.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                  {item.label}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
--- src/features/prescriptions/components/PrescriptionForm.tsx
+++ src/features/prescriptions/components/PrescriptionForm.tsx
@@ -1,6 +1,25 @@
 import { Form } from "@/components/ui/Form";
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+import { Combobox } from "@/components/ui/combobox";
+import { DatePicker } from "@/components/ui/date-picker";
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
 import { z } from "zod";
 
 const prescriptionSchema = z.object({
@@ -20,43 +39,124 @@ const prescriptionSchema = z.object({
         message: "El DNI debe ser un numero",
       }
     ),
-  name: z.string().min(3, {
+  firstName: z.string().min(3, {
     message: "El nombre debe tener al menos 3 caracteres",
   }),
+  lastName: z.string().min(3, {
+    message: "El apellido debe tener al menos 3 caracteres",
+  }),
+  dateOfBirth: z.string().refine(
+    (value) => {
+      return !isNaN(Date.parse(value));
+    },
+    {
+      message: "La fecha de nacimiento debe ser una fecha valida",
+    }
+  ),
+  units: z.coerce
+    .number()
+    .int({
+      message: "Las unidades deben ser un numero entero",
+    })
+    .positive({
+      message: "Las unidades deben ser un numero positivo",
+    }),
 });
 
 export function PrescriptionForm() {
   return (
-    <Form
-      schema={prescriptionSchema}
-      onSubmit={() => {
-        console.log("hola");
-      }}
-      className="w-full max-w-sm grid gap-2 mx-2"
-    >
-      {({ register, formState }) => (
-        <>
-          <Input
-            type="text"
-            placeholder="Correo electronico"
-            {...register("email")}
-            error={formState.errors.email}
-          />
-          <Input
-            type="text"
-            placeholder="DNI"
-            {...register("dni")}
-            error={formState.errors.dni}
-          />
-          <Input
-            type="text"
-            placeholder="Nombre"
-            {...register("name")}
-            error={formState.errors.name}
-          />
-          <Button type="submit">Enviar</Button>
-        </>
-      )}
-    </Form>
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-xl">Crear prescripcion</CardTitle>
+        <CardDescription>
+          Ingresa los datos del paciente para crear una prescripcion y enviarla
+          por correo electronico
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form
+          schema={prescriptionSchema}
+          onSubmit={() => {
+            console.log("hola");
+          }}
+          className="w-full max-w-sm grid gap-2"
+        >
+          {({ register, formState }) => (
+            <>
+              <Input
+                type="text"
+                placeholder="Correo electronico"
+                {...register("email")}
+                error={formState.errors.email}
+              />
+              <Input
+                type="text"
+                placeholder="DNI"
+                {...register("dni")}
+                error={formState.errors.dni}
+              />
+              <Input
+                type="text"
+                placeholder="Nombre"
+                {...register("firstName")}
+                error={formState.errors.firstName}
+              />
+              <Input
+                type="text"
+                placeholder="Apellido"
+                {...register("lastName")}
+                error={formState.errors.lastName}
+              />
+
+              <DatePicker />
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Selecciona una obra social" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Obras Sociales</SelectLabel>
+                    <SelectItem value="osde">OSDE</SelectItem>
+                    <SelectItem value="sm">Swiss Medical</SelectItem>
+                    <SelectItem value="galeno">Galeno</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Selecciona el sexo del paciente" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Sexo del paciente</SelectLabel>
+                    <SelectItem value="osde">Masculino</SelectItem>
+                    <SelectItem value="sm">Femenino</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              <Combobox
+                values={[
+                  { label: "Pastillas", value: "pills" },
+                  { label: "Jarabe", value: "syrup" },
+                  { label: "Inyeccion", value: "injection" },
+                ]}
+                placeholder="Seleccione un medicamento"
+                emptyMessage="No se encontraron medicamentos"
+              />
+
+              <Input
+                type="number"
+                placeholder="Unidades"
+                {...register("units")}
+                error={formState.errors.units}
+                hideArrows
+              />
+              <Textarea placeholder="Escriba su diagnostico aqui" />
+              <Button type="submit">Enviar</Button>
+            </>
+          )}
+        </Form>
+      </CardContent>
+    </Card>
   );
 }
