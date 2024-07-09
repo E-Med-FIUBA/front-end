9cc5b7c051fd91077cc74dd131ba1cc76c28075d
Manuel Sanchez <manuelsfdelavega@gmail.com>
2024-07-09 02:56:52 -0300

    Add mobile hook

--- src/components/ui/DateInput.tsx
+++ src/components/ui/DateInput.tsx
@@ -1,5 +1,7 @@
 import { format, isValid, parse } from "date-fns";
 import { Input } from "./input";
+import React from "react";
+import useCheckMobileScreen from "@/hooks/useCheckMobileScreen";
 
 interface DateInputProps
   extends Omit<
@@ -7,67 +9,65 @@ interface DateInputProps
     "type" | "onBlur"
   > {}
 
-export function DateInput({
-  onChange: onChangeWrapper,
-  ...props
-}: DateInputProps) {
-  const currentDate = new Date();
+export const DateInput = React.forwardRef<HTMLInputElement, DateInputProps>(
+  ({ onChange: onChangeWrapper, ...props }, ref) => {
+    const isMobile = useCheckMobileScreen();
+    const currentDate = new Date();
 
-  const formatDate = (value: string) => {
-    const cleaned = value.replace(/\D+/g, "");
-    const match = cleaned.match(/^(\d{0,2})(\d{0,2})(\d{0,4})$/);
+    const formatDate = (value: string) => {
+      const cleaned = value.replace(/\D+/g, "");
+      const match = cleaned.match(/^(\d{0,2})(\d{0,2})(\d{0,4})$/);
 
-    if (match) {
-      const part1 = match[1];
-      const part2 = match[2] ? "/" + match[2] : "";
-      const part3 = match[3] ? "/" + match[3] : "";
-      return part1 + part2 + part3;
-    }
-    return value;
-  };
+      if (match) {
+        const part1 = match[1];
+        const part2 = match[2] ? "/" + match[2] : "";
+        const part3 = match[3] ? "/" + match[3] : "";
+        return part1 + part2 + part3;
+      }
+      return value;
+    };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const caret = e.target.selectionStart;
-    const value = e.target.value;
-    const formattedDate = formatDate(value);
-    if (formattedDate.length < value.length) {
-      window.requestAnimationFrame(() => {
-        e.target.selectionStart = caret;
-        e.target.selectionEnd = caret;
-      });
-    }
-    e.target.value = formattedDate;
-    onChangeWrapper &&
-      onChangeWrapper(e as React.ChangeEvent<HTMLInputElement>);
-  };
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const caret = e.target.selectionStart;
+      const value = e.target.value;
+      const formattedDate = formatDate(value);
+      if (formattedDate.length < value.length) {
+        window.requestAnimationFrame(() => {
+          e.target.selectionStart = caret;
+          e.target.selectionEnd = caret;
+        });
+      }
+      e.target.value = formattedDate;
+      onChangeWrapper &&
+        onChangeWrapper(e as React.ChangeEvent<HTMLInputElement>);
+    };
 
-  const handleOnBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    const date = parse(value, "dd/MM/yyyy", currentDate);
-    if (isValid(date)) {
-      e.target.value = format(date, "dd/MM/yyyy");
+    const handleOnBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+      const value = e.target.value;
+      const date = parse(value, "dd/MM/yyyy", currentDate);
+      if (isValid(date)) {
+        e.target.value = format(date, "dd/MM/yyyy");
+      } else {
+        e.target.value = "";
+      }
+    };
+
+    if (isMobile) {
+      return (
+        <Input type="date" onChange={onChangeWrapper} {...props} ref={ref} />
+      );
     } else {
-      e.target.value = "";
+      return (
+        <Input
+          type="text"
+          placeholder="DD/MM/YYYY"
+          maxLength={10}
+          {...props}
+          onChange={handleChange}
+          onBlur={handleOnBlur}
+          ref={ref}
+        />
+      );
     }
-  };
-
-  return (
-    <>
-      <Input
-        type="text"
-        placeholder="DD/MM/YYYY"
-        maxLength={10}
-        {...props}
-        onChange={handleChange}
-        onBlur={handleOnBlur}
-        className="hidden md:block"
-      />
-      <Input
-        type="date"
-        className="md:hidden"
-        onChange={onChangeWrapper}
-        {...props}
-      />
-    </>
-  );
-}
+  }
+);
---
+++ src/hooks/useCheckMobileScreen.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const MOBILE_THRESHOLD = 768;
+
+const useCheckMobileScreen = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const handleWindowSizeChange = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  return width <= MOBILE_THRESHOLD;
+};
+
+export default useCheckMobileScreen;
