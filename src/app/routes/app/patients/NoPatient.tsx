c9594bafc0981a6af162f042433277bbe0fc8199
Manuel Sanchez <manuelsfdelavega@gmail.com>
2024-07-21 19:44:00 -0300

    Add instructions for no patient route

--- src/app/routes/app/patients/NoPatient.tsx
+++ src/app/routes/app/patients/NoPatient.tsx
@@ -1,7 +1,13 @@
 export function NoPatientRoute() {
   return (
-    <div className="hidden xl:flex h-full bg-card rounded-md border col-span-3 justify-center items-center">
-      No hay paciente seleccionado
+    <div className="hidden xl:flex xl:flex-col h-full bg-card rounded-md border col-span-3 justify-center items-center">
+      <h3 className="text-lg font-semibold text-center">
+        No hay paciente seleccionado
+      </h3>
+      <p className="text-sm text-muted-foreground">
+        Seleccione uno a traves del menu de la izquierda, o presione el boton de
+        "Agregar paciente" para crear uno nuevo.
+      </p>
     </div>
   );
 }
