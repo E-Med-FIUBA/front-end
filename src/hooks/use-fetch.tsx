8c2566edbe92abdfa5ecb8ec6b9cfe12bcbe265f
Manuel Sanchez <manuelsfdelavega@gmail.com>
2024-10-10 22:00:08 -0300

    add logout on fetch 401

--- src/hooks/use-fetch.tsx
+++ src/hooks/use-fetch.tsx
@@ -1,5 +1,9 @@
 import { useState, useEffect, useCallback } from 'react';
 
+import { ApiError } from '@/lib/api-client';
+
+import { useAuth } from './use-auth';
+
 export const useFetch = <T,>(
   fetchFunction: () => Promise<T>,
 ): {
@@ -11,17 +15,22 @@ export const useFetch = <T,>(
   const [data, setData] = useState<any>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
+  const { logout } = useAuth();
 
   const fetchData = useCallback(async () => {
     try {
       const data = await fetchFunction();
       setData(data);
     } catch (error) {
+      if (error instanceof ApiError && error.status === 401) {
+        logout();
+        return;
+      }
       setError(error as Error);
     } finally {
       setLoading(false);
     }
-  }, [fetchFunction]);
+  }, [fetchFunction, logout]);
 
   useEffect(() => {
     fetchData();
