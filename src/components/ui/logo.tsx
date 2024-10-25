58cce5c93d5d943b3ddb1d2570a97274483576cd
Manuel Sanchez <manuelsfdelavega@gmail.com>
2024-10-25 12:10:19 -0300

    remove logo dark invert

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,7 +1,3 @@
-import { cn } from '@/utils/cn';
-
 export const Logo = ({ className }: { className?: string }) => {
-  return (
-    <img src="./logo.svg" alt="logo" className={cn('dark:invert', className)} />
-  );
+  return <img src="./logo.svg" alt="logo" className={className} />;
 };
